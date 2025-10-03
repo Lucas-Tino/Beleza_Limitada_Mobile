8539f8103a3a85f77e1f74c5e88368e6735494f2
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-10-03 12:07:50 -0300

    feat: navigation routes has been created

---
+++ app/src/main/java/com/example/beleza_limitada_mobile/ui/navigation/Routes.kt
@@ -0,0 +1,26 @@
+package com.example.beleza_limitada_mobile.ui.navigation
+
+object Routes {
+    const val HOME = "home"
+    const val LOGIN_SCREEN = "loginScreen"
+
+    // Telas Manutencao
+    const val DASHBOARD_MANUTENCAO_SCREEN = "dashboardManutencao"
+    const val CREATE_MANUTENCAO_SCREEN = "createManutencaoScreen"
+    const val UPDATE_MANUTENCAO_SCREEN = "updateManutencaoScreen"
+
+    // Telas Percurso
+    const val DASHBOARD_PERCURSO_SCREEN = "dashboardPercurso"
+    const val CREATE_PERCURSO_SCREEN = "createPercursoScreen"
+    const val UPDATE_PERCURSO_SCREEN = "updatePercursoScreen"
+
+    // Telas Veiculo
+    const val DASHBOARD_VEICULO_SCREEN = "dashboardVeiculo"
+    const val CREATE_VEICULO_SCREEN = "createVeiculoScreen"
+    const val UPDATE_VEICULO_SCREEN = "updateVeiculoScreen"
+
+    // Telas Vendedor
+    const val DASHBOARD_VENDEDOR_SCREEN = "dashboardVendedor"
+    const val CREATE_VENDEDOR_SCREEN = "createVendedorScreen"
+    const val UPDATE_VENDEDOR_SCREEN = "updateVendedorScreen"
+}
