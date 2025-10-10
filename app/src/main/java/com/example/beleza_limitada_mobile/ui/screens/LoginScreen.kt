b038203040628db3641470ed0be9ffeb59e1d60a
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-10-10 12:28:57 -0300

    feat: the screen LoginScreen.kt has been started

--- app/src/main/java/com/example/beleza_limitada_mobile/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/beleza_limitada_mobile/ui/screens/LoginScreen.kt
@@ -1,9 +1,157 @@
 package com.example.beleza_limitada_mobile.ui.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.beleza_limitada_mobile.ui.navigation.Routes
+import com.example.beleza_limitada_mobile.ui.theme.Purple
 
 @Composable
 fun LoginScreen(navController: NavController) {
+    val email = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
 
+    val enabled by remember { mutableStateOf(true) }
+
+    val border =
+        if (enabled) {
+            BorderStroke(width = 2.dp, color = Purple)
+        } else {
+            BorderStroke(width = 2.dp, color = Color.Transparent)
+        }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Bem-Vindo!",
+                    fontSize = 24.sp,
+                    color = Purple,
+                    modifier = Modifier.padding(vertical = 30.dp)
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                OutlinedTextField(
+                    value = email.value,
+                    onValueChange = {
+                        email.value = it
+                    },
+                    label = {
+                        Text("Digite o seu email")
+                    },
+                    modifier = Modifier.padding(bottom = 12.dp),
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.White,
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Purple,
+                        focusedLabelColor = Purple,
+                        unfocusedLabelColor = Purple,
+                        focusedIndicatorColor = Purple,
+                        unfocusedIndicatorColor = Purple
+                    ),
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Unspecified, autoCorrectEnabled = false,
+                        imeAction = ImeAction.Next
+                    )
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                OutlinedTextField(
+                    value = password.value,
+                    onValueChange = {
+                        password.value = it
+                    },
+                    label = {
+                        Text("Digite a sua senha")
+                    },
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.White,
+                        unfocusedContainerColor = Color.White,
+                        focusedTextColor = Color.Black,
+                        unfocusedTextColor = Purple,
+                        focusedLabelColor = Purple,
+                        unfocusedLabelColor = Purple,
+                        focusedIndicatorColor = Purple,
+                        unfocusedIndicatorColor = Purple
+                    ),
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Unspecified, autoCorrectEnabled = false,
+                        imeAction = ImeAction.Done
+                    )
+                )
+            }
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                OutlinedButton(
+                    onClick = { navController.navigate(Routes.HOME) },
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Purple
+                    ),
+                    border = border,
+                    modifier = Modifier
+                        .padding(top = 12.dp)
+                ) {
+                    Text("Entrar")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen(navController = rememberNavController())
 }
